069fce6a9fca654d2a4042cd086db12fb565d5d1
Jaen Figueroa <jaenfigueroa03042000@gmail.com>
2024-01-18 14:37:17 -0500

    Update lines-of-lock.sh

--- scripts/lines-of-lock.sh
+++ scripts/lines-of-lock.sh
@@ -1,36 +1,21 @@
-texto=$(git diff --stat origin/dev)
+DIFF_OUTPUT=$(git diff --stat origin/dev)
 
-# ---------------------------------------------------------------------------------
+DIFF_OUTPUT_WITHOUT_LAST_LINE=$(echo "$DIFF_OUTPUT" | head -n -1)
 
-# Eliminar la última línea del texto
-texto_sin_ultima_linea=$(echo "$texto" | head -n -1)
+JSON_STRING="{"
 
-# Inicializar el objeto JSON
-json_string="{"
-
-# Procesar cada línea del texto modificado
 while IFS= read -r line; do
-    # Utilizar expresiones regulares con sed para extraer el nombre del archivo y el valor
     file=$(echo "$line" | sed -n 's/^[[:space:]]*\([^ ]\+\)[[:space:]]*|.*$/\1/p')
     value=$(echo "$line" | sed -n 's/.*|\s*\([0-9]\+\).*$/\1/p')
 
-    # Agregar al objeto JSON
-    json_string="$json_string \"$file\": $value,"
+    JSON_STRING="$JSON_STRING \"$file\": $value,"
 
-done <<< "$texto_sin_ultima_linea"
+done <<< "$DIFF_OUTPUT_WITHOUT_LAST_LINE"
 
-# Eliminar la última coma y cerrar el objeto JSON
-json_string="${json_string%,} }"
+JSON_STRING="${JSON_STRING%,} }"
 
-# ---------------------------------------------------------------------------------
+FILTERED_JSON=$(echo "$JSON_STRING" | jq 'with_entries(select(.key | test("-lock.json$|-lock.yaml$|yarn.lock$|-lockb$")))')
 
-# Filtrar solo los elementos que incluyen "-lock" en el nombre
-filtered_json=$(echo "$json_string" | jq 'with_entries(select(.key | test("-lock.json$|-lock.yaml$|yarn.lock$|-lockb$")))')
+MODIFIED_LOCK_LINES_TOTAL=$(echo "$FILTERED_JSON" | jq '[.[]] | add')
 
-
-# ---------------------------------------------------------------------------------
-
-suma_valores=$(echo "$filtered_json" | jq '[.[]] | add')
-
-# --------------------------------------------------------------------
-echo $suma_valores
+echo $MODIFIED_LOCK_LINES_TOTAL
